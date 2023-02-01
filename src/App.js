833f69e1fbd99a69b7c5357abc4ae290f853c51f
ngdkhuong <nguyenkhuong31072002@gmail.com>
2023-02-01 11:35:18 +0700

    upload client

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/hotel/:id" element={<Hotel />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;

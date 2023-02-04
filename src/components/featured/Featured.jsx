0ed35318796ede1cda476bb1d48b0cd43a32523e
ngdkhuong <nguyenkhuong31072002@gmail.com>
2023-02-04 12:12:17 +0700

    upload featured & propertyList

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -11,7 +11,7 @@ const Featured = () => {
                 />
                 <div className="featuredTitles">
                     <h1>Da Lat</h1>
-                    <h2>2,146 properties</h2>
+                    <h2>2,146 pro3erties</h2>
                 </div>
             </div>
             <div className="featuredItem">
@@ -22,7 +22,7 @@ const Featured = () => {
                 />
                 <div className="featuredTitles">
                     <h1>Vung Tau</h1>
-                    <h2>2,146 properties</h2>
+                    <h2>2,146 prope3ties</h2>
                 </div>
             </div>
             <div className="featuredItem">
@@ -33,6 +33,17 @@ const Featured = () => {
                 />
                 <div className="featuredTitles">
                     <h1>City. Ho Chi Minh</h1>
+                    <h2>2,146 properties</h2>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img
+                    src="https://r-xx.bstatic.com/xdata/images/city/300x240/688844.jpg?k=02892d4252c5e4272ca29db5faf12104004f81d13ff9db724371de0c526e1e15&o="
+                    alt=""
+                    className="featuredImg"
+                />
+                <div className="featuredTitles">
+                    <h1>Da Nang</h1>
                     <h2>2,146 properties</h2>
                 </div>
             </div>
